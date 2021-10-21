dee65066a75a042737e8866f5d04bb15054736a9
Marcel Novotny <marceln@xcroco.com>
2021-10-21 22:35:28 +0200

    Uklid na disku

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 echo nec >/sys/class/rc/rc0/protocols
 echo "@reboot echo nec | sudo tee /sys/class/rc/rc0/protocols" >> /var/spool/cron/crontabs/root
-apt install python3-evdev
+apt install python3-evdev python3-pip
+pip3 install OPi.GPIO
 cp python_mpd.py /usr/bin/
 cp mpdremote.service /etc/systemd/system
 chmod +x /usr/bin/python_mpd.py
